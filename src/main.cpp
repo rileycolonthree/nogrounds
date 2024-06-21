9621b7a54c3bfdb253d18bc6d9096336eb927862
Riley <luna@colonthree.games>
2024-06-21 14:02:51 -0400

    gd semicolon

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@ class $modify(MenuLayer) {
         if (!MenuLayer::init()) return false;
         auto bottomMenu = this->getChildByID("bottom-menu");
         bottomMenu->getChildByID("newgrounds-button")->setVisible(false);
-        bottomMenu->removeChildByID("newgrounds-button")
+        bottomMenu->removeChildByID("newgrounds-button");
         bottomMenu->updateLayout();
         return true;
     }
